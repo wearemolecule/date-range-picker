4fcb3fff382faf4825007b0ca3255401a22470bf
osxi <osxi@users.noreply.github.com>
2016-03-28 18:45:41 -0500

    add blanket; increase mixin test coverage

---
+++ tests/blanket-options.js
@@ -0,0 +1,18 @@
+/* globals blanket, module */
+
+var options = {
+  modulePrefix: 'date-range-picker',
+  filter: '//.*date-range-picker/.*/',
+  antifilter: '//.*(tests|template).*/',
+  loaderExclusions: [],
+  enableCoverage: true,
+  cliOptions: {
+    reporters: ['json'],
+    autostart: true
+  }
+};
+if (typeof exports === 'undefined') {
+  blanket.options(options);
+} else {
+  module.exports = options;
+}
--- tests/unit/mixins/picker-test.js
+++ tests/unit/mixins/picker-test.js
@@ -4,9 +4,33 @@ import { module, test } from 'qunit';
 
 module('Unit | Mixin | picker');
 
-// Replace this with your real tests.
+const PickerObject = Ember.Object.extend(PickerMixin);
+
 test('it works', function(assert) {
-  let PickerObject = Ember.Object.extend(PickerMixin);
   let subject = PickerObject.create();
   assert.ok(subject);
+});
+
+test('converts strings to moments', function(assert) {
+  let dateString = '01/02/3015';
+
+  let subject = PickerObject.create({
+    startDate: dateString,
+    endDate: dateString,
+  });
+
+  assert.equal(subject.get('startDate._isAMomentObject'), true, 'startDate is converted to a moment.');
+  assert.equal(subject.get('endDate._isAMomentObject'), true, 'endDate is converted to a moment.');
+});
+
+test('actions - toggleIsExpanded', function(assert) {
+  let PickerController = Ember.Controller.extend(PickerMixin);
+
+  let subject = PickerController.create({
+    isExpanded: true,
+  });
+
+  subject.send('toggleIsExpanded');
+
+  assert.equal(subject.get('isExpanded'), false, 'isExpanded is toggled to become false.');
 });
