e0b45a6eefd244388265781f77d75c5f80b8a561
osxi <osxi@users.noreply.github.com>
2016-03-28 17:12:25 -0500

    add default blueprint for jquery.inputmask

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 module.exports = {
   name: 'date-range-picker',
   included: function(app) {
-    app.import(app.bowerDirectory + '/jquery.inputmask/dist/min/jquery.inputmask.bundle.min.js');
     this._super.included(app);
+    app.import(app.bowerDirectory + '/jquery.inputmask/dist/min/jquery.inputmask.bundle.min.js');
   }
 };
