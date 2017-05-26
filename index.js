c849c4f6a0fab8d8a9b90a956a2c0311c77547b3
osxi <osxi@users.noreply.github.com>
2017-05-26 10:49:27 -0500

    remove inputmask bower import

--- index.js
+++ index.js
@@ -2,9 +2,5 @@
 'use strict';
 
 module.exports = {
-  name: 'date-range-picker',
-  included: function(app) {
-    this._super.included(app);
-    app.import(app.bowerDirectory + '/jquery.inputmask/dist/min/jquery.inputmask.bundle.min.js');
-  }
+  name: 'date-range-picker'
 };
